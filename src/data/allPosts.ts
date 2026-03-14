7d69172eba03a8287730a5d412b8d3963d70afad
Lê Vĩnh Khang <ToshikoSteinhaus01544@hotmail.com>
2026-03-14 15:00:00 +0700

    feat: implement blog post metadata aggregation

---
+++ src/data/allPosts.ts
@@ -0,0 +1,11 @@
+import { mdxPosts } from '../lib/mdxPosts'
+import { posts } from './posts'
+import type { PostMeta } from './posts'
+
+export type { PostMeta }
+
+function sortByDate(a: PostMeta, b: PostMeta): number {
+  return new Date(b.date).getTime() - new Date(a.date).getTime()
+}
+
+export const allPosts: PostMeta[] = [...posts, ...mdxPosts].sort(sortByDate)
