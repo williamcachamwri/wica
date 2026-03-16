6b81a935eab4f9b365d0912c0665ec84cb5ac6d2
Lê Vĩnh Khang <ToshikoSteinhaus01544@hotmail.com>
2026-03-16 11:00:00 +0700

    feat: create animated InlineLink component

---
+++ src/components/InlineLink.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from 'react'
+
+interface InlineLinkProps {
+  href: string
+  children: ReactNode
+}
+
+export function InlineLink({ href, children }: InlineLinkProps) {
+  return (
+    <a href={href} className="inline-link">
+      {children}
+    </a>
+  )
+}
