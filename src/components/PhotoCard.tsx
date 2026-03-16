da947744c1c46c85fc9ded8a9045b3c732fa0677
Lê Vĩnh Khang <ToshikoSteinhaus01544@hotmail.com>
2026-03-16 14:30:00 +0700

    feat: create PhotoCard polaroid component

---
+++ src/components/PhotoCard.tsx
@@ -0,0 +1,12 @@
+import type { Photo } from '../types'
+
+type PhotoCardProps = Photo
+
+export function PhotoCard({ src, caption, rotate }: PhotoCardProps) {
+  return (
+    <figure className="polaroid" style={{ '--rotate': `${rotate}deg` } as React.CSSProperties}>
+      <img src={src} alt={caption} loading="lazy" />
+      <figcaption>{caption}</figcaption>
+    </figure>
+  )
+}
