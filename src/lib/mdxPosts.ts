df308db9b3607107babd42513b86aa1e573b01a4
Lê Vĩnh Khang <ToshikoSteinhaus01544@hotmail.com>
2026-03-19 09:00:00 +0700

    feat: set up MDX blog post processing

---
+++ src/lib/mdxPosts.ts
@@ -0,0 +1,14 @@
+import type { PostMeta } from '../data/posts'
+
+interface MdxModule {
+  default: React.ComponentType
+  meta: PostMeta
+}
+
+const modules = import.meta.glob<MdxModule>('../posts/*.mdx', { eager: true })
+
+export const mdxPosts: PostMeta[] = Object.values(modules).map((mod) => mod.meta)
+
+export function getMdxPost(slug: string): MdxModule | undefined {
+  return Object.values(modules).find((mod) => mod.meta.slug === slug)
+}
