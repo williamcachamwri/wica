c2c2536e6c8d24c74fbb414d7448ae2f4c551b8c
Lê Vĩnh Khang <ToshikoSteinhaus01544@hotmail.com>
2026-03-19 09:30:00 +0700

    feat: add Math component for LaTeX rendering

---
+++ src/mdx/Math.tsx
@@ -0,0 +1,29 @@
+import katex from 'katex'
+
+interface MathProps {
+  children: string
+  block?: boolean
+}
+
+export function Math({ children, block = false }: MathProps) {
+  const html = katex.renderToString(children, {
+    throwOnError: false,
+    displayMode: block,
+  })
+
+  if (block) {
+    return (
+      <div
+        className="math-block"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    )
+  }
+
+  return (
+    <span
+      className="math-inline"
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  )
+}
