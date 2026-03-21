6631969b68d783c38f7981603fd1c9ce2bbb058b
Lê Vĩnh Khang <ToshikoSteinhaus01544@hotmail.com>
2026-03-21 12:00:00 +0700

    feat: build garden element renderer

---
+++ src/features/garden/components/pixel-garden-element-renderer.tsx
@@ -0,0 +1,57 @@
+import { memo } from "react";
+import { PixelRenderer } from "../../../components/PixelRenderer";
+import { useGardenElementAnimation } from "../hooks/use-garden-element-animation";
+import { GARDEN_ELEMENTS } from "../utils/pixel-garden-elements";
+import type { GardenElementType, PixelFrame } from "../types";
+
+type PixelGardenElementRendererProps = {
+  element: GardenElementType;
+  scale?: number;
+  className?: string;
+};
+
+function PixelGardenElementRendererImpl({
+  element,
+  scale = 3,
+  className,
+}: PixelGardenElementRendererProps) {
+  const config = GARDEN_ELEMENTS[element];
+  if (config.frames.length <= 1) {
+    return <StaticGardenElementRenderer element={element} scale={scale} className={className} />;
+  }
+  return <AnimatedGardenElementRenderer element={element} scale={scale} className={className} />;
+}
+
+type VariantProps = {
+  element: GardenElementType;
+  scale: number;
+  className?: string;
+};
+
+const StaticGardenElementRenderer = memo(({ element, scale, className }: VariantProps) => {
+  const { palette, gridWidth, gridHeight, frames } = GARDEN_ELEMENTS[element];
+  const frame = frames[0] ?? [] as PixelFrame;
+  return (
+    <PixelRenderer
+      config={{ palette, gridWidth, gridHeight }}
+      frame={frame}
+      scale={scale}
+      className={className}
+    />
+  );
+});
+
+const AnimatedGardenElementRenderer = memo(({ element, scale, className }: VariantProps) => {
+  const config = GARDEN_ELEMENTS[element];
+  const frame = useGardenElementAnimation(config);
+  return (
+    <PixelRenderer
+      config={{ palette: config.palette, gridWidth: config.gridWidth, gridHeight: config.gridHeight }}
+      frame={frame}
+      scale={scale}
+      className={className}
+    />
+  );
+});
+
+export const PixelGardenElementRenderer = memo(PixelGardenElementRendererImpl);
